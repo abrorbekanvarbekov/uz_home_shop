f720f7c4707b0e26030885d2a6ad9dfcf67ea783
abror <1aabror@gmail.com>
2022-11-13 22:07:22 +0900

    host chuga

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure--+p#$*u6dlkhsow9k)tq(!ebap^hkysce9ac9%2&_d#x28j_ax
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
